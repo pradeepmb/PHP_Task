cef3de5fd5a63ee1e78f7f21f8ca8e91988812eb
Pradeep <pradeep@appinessworld.com>
2019-12-12 10:23:50 +0530

    Files
    Created another files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <form> 
+            <table>
+                <tr><td>Name:</td><td><input type="text" name="name"></td></tr>  
+                <tr><td>Email:</td><td><input type="email" name="emai"></td></tr>
+                <tr><td>Mobile:</td><td><input type="text" name="mobile"></td></tr>   
+                <tr><td>Message:</td><td><textarea name="message" cols="25" rows="5"></textarea></td></tr>    
+                <tr><td>Mode of communication:</td><td><input type="checkbox" name="moc[]">Phone</td><td><input type="checkbox" name="moc[]">Email</td><input type="checkbox" name="moc[]"><td></td></tr>       
+            </table>
+        </form>
+    </body>
+</html>
+<script src="ajax.js"></script>
